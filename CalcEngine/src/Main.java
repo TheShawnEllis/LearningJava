61312140fd5268d2ea63d50162411c84d750375f
DeShawn <xeno2188@gmail.com>
2024-03-26 16:02:42 -0700

    Block statement added to the calcengine

--- CalcEngine/src/Main.java
+++ CalcEngine/src/Main.java
@@ -1,20 +1,28 @@
-public class Main {
-    public static void main(String[] args) {
+public class Main 
+{
+    public static void main(String[] args) 
+    {
         double value1 = 100.0d;
-        double value2 = 50.0d;
+        double value2 = 0.0d;
         double result = 0.0d;
-        char opCode = 'a';
+        char opCode = 'd';
 
-        if(opCode == 'a') 
+        if (opCode == 'a')
             result = value1 + value2;
-        else if(opCode == 's') 
+        else if (opCode == 's')
             result = value1 - value2;
-        else if(opCode == 'm') 
+        else if (opCode == 'm')
             result = value1 * value2;
-        else if(opCode == 'd') 
-            result = value1 / value2;
+        else if (opCode == 'd')
+        {
+            if (value2 != 0) 
+                result = value1 / value2;
+        }
         else 
+        {
+            System.out.println("Invalid opCode: " + opCode);
             result = 0.0d;
+        }
 
         System.out.println(result);
     }

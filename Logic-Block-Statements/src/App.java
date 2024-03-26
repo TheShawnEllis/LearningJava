83f6a80b5a0985fbad637397a0a6e7d17465ddff
DeShawn <xeno2188@gmail.com>
2024-03-26 09:52:21 -0700

    Completed conditional logic and if-else statement lesson

---
+++ Logic-Block-Statements/src/App.java
@@ -0,0 +1,30 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        ////// Conditional Logic and Relational Operators //////
+        // Relational Operators
+            // > Greater than
+            // < Less than
+            // >= Great than or equal to
+            // <= Less than or equal to
+            // == Equal to 
+            // != Not equal to 
+
+        // Conditional Assignment - returns value based on result of condition
+            // result = condition ? tre-value : false-value;
+        
+        int value1 = 7;
+        int value2 = 5;
+        int maxValue = (value1 > value2) ? value1 : value2;
+        System.out.println(maxValue);
+
+        ////// If-Else Statement //////
+        if (value1 > value2)
+        {
+            System.out.println("Value1 is bigger");
+        }
+        else
+        {
+            System.out.println("Value2 is bigger");
+        }
+    }
+}
